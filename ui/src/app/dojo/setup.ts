e68a93a3346e7659e2518ef81d4295dd2a55bc01
Starknet Dev <starknetdev@gmail.com>
2024-09-09 20:26:18 -0400

    add feeTokenAddress

--- ui/src/app/dojo/setup.ts
+++ ui/src/app/dojo/setup.ts
@@ -21,7 +21,8 @@ export async function setup({ rpcUrl, network, setCreateBurner }: SetupProps) {
     ),
     accountClassHash: networkConfig[network!].accountClassHash,
     rpcProvider: dojoProvider,
-    feeTokenAddress: "",
+    feeTokenAddress:
+      "0x49d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7",
   });
 
   await burnerManager.init();
@@ -41,10 +42,8 @@ export async function setup({ rpcUrl, network, setCreateBurner }: SetupProps) {
 
   return {
     config: {
-      masterAddress:
-        networkConfig[network!].masterAccount,
-      masterPrivateKey:
-        networkConfig[network!].masterPrivateKey,
+      masterAddress: networkConfig[network!].masterAccount,
+      masterPrivateKey: networkConfig[network!].masterPrivateKey,
     },
     burnerManager,
     dojoProvider,
